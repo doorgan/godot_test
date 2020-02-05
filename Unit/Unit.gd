5addd2359b10757672c678ff20d4c063d72c6ad3
dorgan <dorgandash@gmail.com>
2020-02-05 03:10:32 -0300

    saving changes

--- Unit/Unit.gd
+++ Unit/Unit.gd
@@ -1,12 +1,34 @@
 extends KinematicBody2D
 class_name Unit
 
+export (NodePath) var effects_slot : NodePath
+
+onready var EffectsSlot : Node = get_node(effects_slot)
+
 var effects : = []
 
-func add_effect(effect) -> void:
-	
+func _ready():
+	assert(effects_slot)
 
 
-func remove_effect(effect) -> String:
-	if effect in effects:
-		effects.find()
+func add_effect(effect : Node) -> void:
+	# TODO: use an Effect node as type
+	# TODO: check duplicates
+	EffectsSlot.add_child(effect)
+
+
+# Removes the effect from the unit.
+# Should take a Node, a NodePath or a custom resource?
+func remove_effect(effect_name : NodePath) -> void:
+	EffectsSlot.get_node(effect_name).finish()
+
+# Returns the current active effects on the unit
+# Should return an array of nodes or an array of custom resources?
+func get_active_effects() -> Array:
+	return EffectsSlot.get_children()
+
+func has_effect(effect_name):
+	for effect in get_active_effects():
+		if effect.name == effect_name:
+			return true
+	return false
